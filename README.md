357bc75a8ea8074d5ab2f0ca5792d94476ebc00e
Luciano Porto <luciano.bapo@gmail.com>
2016-06-16 01:21:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# erpnet-file-manager
+
+[Site do GitHub](https://github.com/lucianobapo/erpnet-file-manager)
+
+[![Latest Stable Version](https://poser.pugx.org/ilhanet/erpnet-file-manager/v/stable)](https://packagist.org/packages/ilhanet/erpnet-file-manager) 
+[![Total Downloads](https://poser.pugx.org/ilhanet/erpnet-file-manager/downloads)](https://packagist.org/packages/ilhanet/erpnet-file-manager) 
+[![Latest Unstable Version](https://poser.pugx.org/ilhanet/erpnet-file-manager/v/unstable)](https://packagist.org/packages/ilhanet/erpnet-file-manager) 
+[![License](https://poser.pugx.org/ilhanet/erpnet-file-manager/license)](https://packagist.org/packages/ilhanet/erpnet-file-manager)
---
+++ src/FileManager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luciano
+ * Date: 16/06/16
+ * Time: 00:21
+ */
+
+namespace ErpNET\FileManager;
+
+
+use Illuminate\Support\Facades\Storage;
+
+class FileManager
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\File\UploadedFile|array $uploadedFile
+     * @param string $fileDir
+     * @param string $newName
+     * @return bool
+     */
+    public function saveFile($uploadedFile, $fileDir, $newName = null){
+        if ($uploadedFile->isValid()){
+            if (substr($fileDir,-1)!=DIRECTORY_SEPARATOR)
+                $fileDir = $fileDir . DIRECTORY_SEPARATOR;
+
+            if (!Storage::exists($fileDir)) Storage::makeDirectory($fileDir);
+
+            if (is_null($newName))
+                $fileName = $uploadedFile->getFilename();
+            else
+                $fileName = $newName . '.' . $uploadedFile->getClientOriginalExtension();
+
+            return Storage::put($fileDir . $fileName, file_get_contents($uploadedFile->getRealPath()));
+        } else return false;
+    }
+}
